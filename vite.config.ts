3476b6161fc3bfdb2b3a2dba3522550c4d4c564b
Narcissus-Ma <103873240+Narcissus-Ma@users.noreply.github.com>
2025-08-28 23:38:01 +0800

    feat: 设置路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/resume-site/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
