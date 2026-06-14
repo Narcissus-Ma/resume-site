67b5f89d686a9c6770302a3fb92fcf75fc2a2d2c
Narcissus-Ma <103873240+Narcissus-Ma@users.noreply.github.com>
2026-06-14 12:46:52 +0800

    feat: 调整局域网访问命令

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   // 在开发环境使用'/resume-site/'，在生产环境使用相对路径
   base: process.env.NODE_ENV === 'production' ? './' : '/resume-site/',
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
   plugins: [
     react({
       babel: {
